3ad8757f68879322c8f4a59ae426ab6cb8cd147d
Kanan F. Ahmadli <Kanan.Ahmadli@kapitalbank.az>
2025-03-24 14:58:57 +0400

    - test

--- src/main/java/com/company/messaging/consumer/AvroConsumer.java
+++ src/main/java/com/company/messaging/consumer/AvroConsumer.java
@@ -1,18 +1,16 @@
-//package com.company.messaging.consumer;
-//
-//import KB.TLG;
-//import org.apache.kafka.clients.consumer.ConsumerRecord;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class AvroConsumer {
-//
-//    @KafkaListener(topics = "${avro.topic.name}", containerFactory = "kafkaListenerContainerFactory")
-//    public void read(ConsumerRecord<String, TLG> record) {
-//        String key = record.key();
-//        TLG event = record.value();
-//        System.out.println("Avro message received for key : " + key + " value : " + event.toString());
-//    }
-//
-//}
+package com.company.messaging.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AvroConsumer {
+
+    @KafkaListener(topics = "cms.A4M.TEXTRACTDELAY")
+    public void read(ConsumerRecord<String, String> record) {
+//        org.apache.kafka.common.serialization
+        System.out.println("Avro message received for value : " + record.value());
+    }
+
+}
